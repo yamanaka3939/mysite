72e2c71c22f8a25060a85601451bbdd710b40f46
yamanaka3939 <140675194+yamanaka3939@users.noreply.github.com>
2023-07-27 13:41:57 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+{
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+
+  btn.addEventListener('click', () => {
+    const n = Math.floor(Math.random() * 3);
+
+    switch (n) {
+      case 0:
+        result.textContent = '大吉';
+        break;
+      case 1:
+        result.textContent = '中吉';
+        break;
+      case 2:
+        result.textContent = '凶';
+        break;
+    }
+  });
+}
